c86f05405a6ab41d8341ad1cfd1004975f84b222
doug-yan <dyan@nevada.unr.edu>
2014-12-11 12:45:16 -0800

    Turn in version.

--- tempdes.c
+++ tempdes.c
@@ -6,32 +6,51 @@
 
 int main(int argc[], char *argv[])
 {
-	if(argc != 3)
+	int k;
+	char rbuff[4000];
+	char in1[1000];
+	char in2[1000];
+	char in3[1000];
+	char in4[1000];
+
+	//File i/o
+	FILE *fp;
+	char *filename = argv[3];
+	fp = fopen(filename, "r");
+
+	if(fp == NULL)
 	{
-		printf("Error in command line input.");
+		printf("Error opening file. \n");
 		return 0;
 	}
 
-	int k;
-	char in[1000];
+	char ch;
+	while( (ch = fgetc(fp) ) != EOF)
+	{
+		rbuff[bufferCounter] = ch;
+		bufferCounter++;
+	}
+	fclose(fp);
 
-	//File io
-	FILE *fp;
+	int counter = bufferCounter / 4, i;
+	for(i = 0; i < counter; i++)
+		in1[i] = rbuff[i];
+	for(i = 0; i < counter; i++)
 
-	static unsigned char cbc_key[8] = {0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef};
+
+	static unsigned char cbc_key[16] = argv[2];
 	des_key_schedule key;
 
 	if ((k = des_set_key_checked(&cbc_key,key)) != 0)
 		printf("\nkey error\n");
 
-	in[0] = 3212314;
-	in[1] = 1231233;
 
-	printf("DES Clear Text: %ld%ld\n",in[0],in[1]);
+
+	printf("DES Clear Text: %s\n",rbuff);
+	
 	des_encrypt1(in,key,ENC);
 
 	printf("DES Encryption: %u%u\n",in[0],in[1]);
 
     	des_encrypt1(in,key,DEC);
-    printf("DES Decryptoin: %ld%ld\n", in[0],in[1]);
 }
